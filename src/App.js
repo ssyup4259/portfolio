a05bcd2a7e2fae029b98624d9efe841a69e7e4b4
ssyup4259 <ssyup8717@gmail.com>
2020-02-23 00:20:19 +0900

    react study

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+function Food({ fav }) {
+  return <h1> i lipe {fav} </h1>
+}
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <div>hello</div>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiposal" />
+      <Food fav="chukumi" />
+    </div>
+  );
 }
 
 export default App;
