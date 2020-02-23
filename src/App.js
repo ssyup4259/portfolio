b634869bbdb808956fbddeae75daa9462dfc2957
ssyup4259 <ssyup8717@gmail.com>
2020-02-23 21:26:19 +0900

    react study

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
+//(props)  -> Object 들어간다.
+// props.fav  -> 아래에서 지정해준 fav가 들어간다.
 function Food({ fav }) {
   return <h1> i lipe {fav} </h1>
 }
+
+const foodILike = [{}]
 
 function App() {
   return (
     <div>
       <div>hello</div>
       <Food fav="kimchi" />
-      <Food fav="ramen" />
+      <Food fav="ramenn" />
       <Food fav="samgiposal" />
       <Food fav="chukumi" />
     </div>
