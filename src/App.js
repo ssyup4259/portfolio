72706e6b85a93900ac38e6742d9295f1f4a5e536
ssyup4259 <ssyup8717@gmail.com>
2020-02-25 01:14:23 +0900

    react router study

--- src/App.js
+++ src/App.js
@@ -1,60 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import axios from 'axios';
-import Movie from './Movie';
-import './Movie.css';
-
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  //async : javascript 에게 componentDidMount 가끝날때까지 기다릴라고 명령
-  getMovies = async () => {
-    // const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    // console.log(movies.data.data.movies);
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    this.setState({ movies, isLoading: false });
-    console.log(movies);
-  }
-  componentDidMount() {
-    // setTimeout(() => {
-    //   this.setState({ isLoading: false });
-    // }, 6000);
-    // axios 는 fetch 위에 있는 작은 layer
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return <section className="container">
-      {isLoading ? (
-        <div className="loader">
-          <span className="loader__text">Loading...</span>
-        </div>
-      ) : (
-          <div class="movies">
-            {movies.map(movie => (
-              <Movie
-                key={movie.key}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image} 
-                genres={movie.genres}/>
-            ))}
-          </div>
-        )}
-    </section>
-  };
+import { HashRouter, Route, BrowserRouter } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
+function App() {
+  return (
+    // 두개를 동시에 렌더링 하려면 fragment vlfdy
+    <>
+    {/* BrowserRouter 써도되는데 애는 /#/ 가 없어서 github page 에서 조금 힘듬 */}
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+    <footer></footer>
+    </>
+  );
 }
 
 export default App;
-
-// javascript 내에서 class 는 본인(Component class) 을 의미해서
-// 헷갈릴 경우가 있따. className을 쓴다.
